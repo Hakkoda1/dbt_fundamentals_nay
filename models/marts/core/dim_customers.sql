39955b1d9754d70314aa60f1c704df40fe55561e
Nay Zaw Aung Win <nay_win@hakkoda.io>
2023-01-28 06:55:23 +0000

    combined fct_customer_orders.sql and dim_customers.sql and deleted fct_customer_orders.sql

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -4,11 +4,27 @@ with customers as (
 
 ),
 
-customer_orders as (
+orders as (
 
-    select * from {{ ref('fct_customer_orders') }}
+    select * from {{ ref('stg_orders') }}
 
 ),
+
+customer_orders as (
+
+    select
+        customer_id,
+
+        min(order_date) as first_order_date,
+        max(order_date) as most_recent_order_date,
+        count(order_id) as number_of_orders
+
+    from orders
+
+    group by 1
+
+),
+
 
 final as (
 
