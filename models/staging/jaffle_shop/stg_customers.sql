5ff0028e3d5d3d530859950cc689e48727b0f273
Nay Zaw Aung Win <nay_win@hakkoda.io>
2023-01-28 07:07:56 +0000

    created staging/stripe/_src.yml and staging/stripe/stg_payments.sql

--- models/staging/jaffle_shop/stg_customers.sql
+++ models/staging/jaffle_shop/stg_customers.sql
@@ -1,6 +1,7 @@
 with customers as (
 
     select
+    
         id as customer_id,
         first_name,
         last_name
---
+++ models/staging/stripe/stg_payments.sql
@@ -0,0 +1,17 @@
+with payments as (
+
+    select
+
+        id as payment_id,
+        orderid as order_id,
+        paymentmethod as payment_method,
+        status as payment_status,
+        amount as payment_amount,
+        created as payment_date,
+        _batched_at
+
+    from {{ source('stripe', 'payment') }}
+
+)
+
+select * from payments
